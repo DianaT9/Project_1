ecf1de2147b2f64a7c1313379a82c5f842678d86
Diana <dianatrili9@gmail.com>
2020-01-14 11:09:14 +0200

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+/**
+ * A class that describes a User Model on a Social Media Platform
+ */
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    /**
+     * Creates a new {@link User} with provided name, email and a random ID.
+     *
+     * @param name  - User name
+     * @param email - User email
+     */
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.id = new Random().nextInt();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.id = new Random().nextInt();
+    }
+}
+
